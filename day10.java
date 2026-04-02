22b570c66102becbc9636be5af0c793189606be6
Rahul Kumar <rahulkumarchoudhary347@gmail.com>
2026-04-02 21:16:10 +0530

    new  add

--- day10.java
+++ day10.java
@@ -33,33 +33,70 @@
 
 // question in java  on 2d  array in java  
 
-import java.util.*;
+// import java.util.*;
 
-public class day10 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int row = sc.nextInt();
-        int colms = sc.nextInt();
+// public class day10 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int row = sc.nextInt();
+//         int colms = sc.nextInt();
 
-        int number[][] = new int[row][colms];
+//         int number[][] = new int[row][colms];
 
-        // for input of row and colom
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < colms; j++) {
-                number[i][j] = sc.nextInt();
-            }
+//         // for input of row and colom
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < colms; j++) {
+//                 number[i][j] = sc.nextInt();
+//             }
 
-        }
-        int x = sc.nextInt();
+//         }
+//         int x = sc.nextInt();
 
-        // for output from user in JAVA in 2d array
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < colms; j++) {
-                if (number[i][j] == x) {
-                    System.out.println((i + " " + j));
-                }
-            }
-            System.out.println();
-        }
-    }
-}
+//         // for output from user in JAVA in 2d array
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < colms; j++) {
+//                 if (number[i][j] == x) {
+//                     System.out.println((i + " " + j));
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// questino in java  of  marteix  in 2d array 
+
+// import java.util.*;
+
+// public class day10 {
+//     public static void main(String[]args) {
+
+//         // input for user of row and colm
+
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int m = sc.nextInt();
+
+//         int matrix[][] = new int[n][m];
+
+//         // for input of matrix in 2d array in java
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 matrix[i][j] = sc.nextInt();
+
+//             }
+
+//         }
+//         System.out.println("transpose of matrix");
+
+//         // for output of tranpose in java in 2d array
+//         for (int j = 0; j < m ; j++) {
+//             for (int i = 0; i< n; i++) {
+//                 System.out.print(matrix[i][j] + "");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
