7340001d7cad6199319f8c95eb7013bd819ab602
Rahul Kumar <rahulkumarchoudhary347@gmail.com>
2026-04-03 12:39:27 +0530

    new add

---
+++ dayy10.java
@@ -0,0 +1,267 @@
+// array question in java 
+
+// question 1
+
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input from user in java
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int number[] = new int[n];
+
+//         // for out put
+//         for (int i = 0; i < n; i++) {
+//             number[i] = sc.nextInt();
+//         }
+//         for (int i = 0; i < n; i++) {
+//             System.out.println(number[i]);
+//         }
+
+//     }
+// }
+
+// question 2 in java  
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         int number[] = new int[n];
+
+//         // for number input
+//         for (int i = 0; i < n; i++) {
+//             number[i] = sc.nextInt();
+//         }
+
+//         for (int i = 0; i < n; i++) {
+
+//             System.out.println(n * (n + 1) / 2);
+
+//         }
+//     }
+// }
+
+// question in java  
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         System.out.println(n * (n + 1) / 2);
+//     }
+// }
+
+// question in 3 
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int number[] = new int[size];
+
+//         // for input of number in array
+//         for (int i = 0; i < size; i++) {
+//             number[i] = sc.nextInt();
+//         }
+//         int max = number[0];
+//         int min = number[0];
+//         // for output of array in java
+//         for (int i = 0; i < size; i++) {
+//             if (number[i] > max) {
+//                 max = number[i];
+//             }
+//             if (number[i] < min) {
+//                 min = number[i];
+//             }
+//         }
+
+//         System.out.println(+max);
+//         System.out.println(+min);
+
+//     }
+// }
+
+// question in 3 
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         System.out.println(n * (n + 1) / 2);
+//     }
+// }
+
+// question in 3 
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int number[] = new int[size];
+
+//         // for input of number in array
+//         for (int i = 0; i < size; i++) {
+//             number[i] = sc.nextInt();
+//         }
+//         int max = number[0];
+//         int min = number[0];
+//         // for output of array in java
+//         for (int i = 0; i < size; i++) {
+//             if (number[i] > max) {
+//                 max = number[i];
+//             }
+//             if (number[i] < min) {
+//                 min = number[i];
+//             }
+//         }
+
+//         System.out.println(+max);
+//         System.out.println(+min);
+
+//     }
+// }
+
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         System.out.println(n * (n + 1) / 2);
+//     }
+// }
+
+// question in 3 
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int number[] = new int[size];
+
+//         // for input of number in array
+//         for (int i = 0; i < size; i++) {
+//             number[i] = sc.nextInt();
+//         }
+//         int max = number[0];
+//         int min = number[0];
+//         // for output of array in java
+//         for (int i = 0; i < size; i++) {
+//             if (number[i] > max) {
+//                 max = number[i];
+//             }
+//             if (number[i] < min) {
+//                 min = number[i];
+//             }
+//         }
+
+//         System.out.println(+max);
+//         System.out.println(+min);
+
+//     }
+// }
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         System.out.println(n * (n + 1) / 2);
+//     }
+// }
+
+// question in 3 
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int number[] = new int[size];
+
+//         // for input of number in array
+//         for (int i = 0; i < size; i++) {
+//             number[i] = sc.nextInt();
+//         }
+//         int max = number[0];
+//         int min = number[0];
+//         // for output of array in java
+//         for (int i = 0; i < size; i++) {
+//             if (number[i] > max) {
+//                 max = number[i];
+//             }
+//             if (number[i] < min) {
+//                 min = number[i];
+//             }
+//         }
+
+//         System.out.println(+max);
+//         System.out.println(+min);
+
+//     }
+// }
+// import java.util.*;
+
+// public class dayy10 {
+//     public static void main(String[] args) {
+//         // for input
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         System.out.println(n * (n + 1) / 2);
+//     }
+// }
+
+// question in 3 
+import java.util.*;
+
+public class dayy10 {
+    public static void main(String[] args) {
+        // for input
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int number[] = new int[size];
+
+        // for input of number in array
+        for (int i = 0; i < size; i++) {
+            number[i] = sc.nextInt();
+        }
+        int odd = 0;
+        int even = 0;
+        // for output of array in java
+        for (int i = 0; i < size; i++) {
+            if (number[i] % 2 == 0) {
+                even++;
+            }
+            if (number[i] % 2 != 0) {
+                odd++;
+            }
+        }
+
+        System.out.println(+even);
+        System.out.println(+odd);
+
+    }
+}
