c6c652e91f6f188e535e17d0a736f6fd27960c0f
Rahul Kumar <rahulkumarchoudhary347@gmail.com>
2026-04-07 17:34:22 +0530

    new  add

--- day12.java
+++ day12.java
@@ -1,5 +1,26 @@
-public class day12{
+// binary opertor  in java  
+// public class day12{
+//     public static void main(String[]args){
+//         int a = 10;
+//         int b = 0;
+
+//         b = ++a;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// }
+
+// bit manupliCTION IN JAVA
+public class  day12{
     public static void main(String[]args){
-        System.out.println("hello");
+        int n = 5;
+        int pos = 3;
+        int bitMask = 1;
+
+        if((bitMask & n )== 0 ){
+            System.out.println("bit was  zero");
+        }else{
+            System.out.println("bit was  one v");
+        }
     }
 }
